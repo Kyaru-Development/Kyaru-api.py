6afce490030bb8ea3ba9d19ed4c701d516ff3f9d
Kotik <62649860+XDENIX@users.noreply.github.com>
2020-12-05 16:50:12 +0400

    aaaaaaaaa

---
+++ index.py
@@ -0,0 +1,24 @@
+import requests 
+ 
+class KyaruAgent :
+    def __init__(self, api_token=None):
+        self.key = api_token
+        if not self.key:
+            raise Exception("Specify API-KEY. Documentation - https://github.com/Team-Sakura/Kyaru-API.PY")
+
+    def get(self, endpoint=None):
+
+        if not endpoint:
+            raise Exception("Specify an available endpoint. Documentation - https://github.com/Team-Sakura/Kyaru-API.PY")
+
+        return req(endpoint, headers = 
+            { 
+            "Authorization": f"{self.key}" 
+            } 
+        )
+
+def req(endpoint, headers):
+    res = requests.get(f"https://kyaru-api.glitch.me/api/v1/{endpoint}", headers=headers)
+    return res.json()
+
+# Developer with ❤ by Kyaru Development #
